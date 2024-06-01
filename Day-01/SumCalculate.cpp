da5c9d15255077fa7638afeaaddf72b6c4956312
Rajiya Sultana <rssumu04@gmail.com>
2024-06-02 00:54:00 +0600

    Create SumCalculate.cpp

---
+++ Day-01/SumCalculate.cpp
@@ -0,0 +1,7 @@
+//This program will take three integer as input. And check that the sum of two number is equal to another one.
+#include<iostream>
+using namespace std;
+
+int main(){
+    return 0;
+}
