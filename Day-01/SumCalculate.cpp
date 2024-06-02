93daacb2f02a98aceeb22e89e58fe8007e20f798
Rajiya Sultana <rssumu04@gmail.com>
2024-06-02 11:00:18 +0600

    1st file

--- Day-01/SumCalculate.cpp
+++ Day-01/SumCalculate.cpp
@@ -1,7 +1,18 @@
 //This program will take three integer as input. And check that the sum of two number is equal to another one.
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        int a,b,c;
+        cin >>a>>b>>c;
+        if(a+b == c || b+c == a || a + c == b){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
