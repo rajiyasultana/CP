3aefa9e538b58a8c267bf41688de0f4d08cb59cf
Rajiya Sultana <rssumu04@gmail.com>
2024-06-09 11:47:21 +0600

    Update WordCapitalization_CF.cpp

--- Day-07/WordCapitalization_CF.cpp
+++ Day-07/WordCapitalization_CF.cpp
@@ -1,6 +1,13 @@
-#include<bits/stdc++.h>
+//This problem is to capitalize first charecter from a word.
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
+    string s; 
+    cin >> s; //taking a string
+    s[0] = toupper(s[0]); //cpitalizing first character into upper case using toupper() function. this is belongs to "cctype" headerfile.
+
+    cout << s << endl;
+
     return 0;
 }
