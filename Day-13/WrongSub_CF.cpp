771aa0934f7518db58e1ebca5fe0e337698e7960
Rajiya Sultana <rssumu04@gmail.com>
2024-06-14 20:06:41 +0600

    Update WrongSub_CF.cpp

--- Day-13/WrongSub_CF.cpp
+++ Day-13/WrongSub_CF.cpp
@@ -1,5 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<k; i++){
+        int temp = n % 10;
+        if(temp > 0){
+            n -=1;
+        }
+        if (temp == 0){
+            n /=10;
+        }
+    }
+    cout << n;
     return 0;
 }
