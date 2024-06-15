c7ac1448d8e5f43ceca1882ab745f10412d18824
Rajiya Sultana <rssumu04@gmail.com>
2024-06-15 15:29:01 +0600

    Update TwoSum_LC.cpp

--- Day-13/TwoSum_LC.cpp
+++ Day-13/TwoSum_LC.cpp
@@ -1,5 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
+    string nums;
+    int target;
+    cin >> nums;
+    cin >> target;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for(int j= i+1; j<nums.size(); j++){
+            if(nums[i] == nums[j]){
+                cout << i << " " << j;
+            }
+        }
+        
+    }
     return 0;
 }
