ac8fa70ac5baa5d16662f8e49cdd21ba50dffcd4
Rajiya Sultana <rssumu04@gmail.com>
2024-06-16 22:57:00 +0600

    Update Translation.cpp

--- Day-15/Translation.cpp
+++ Day-15/Translation.cpp
@@ -1,5 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
+    string s, t;
+    int count = 0;
+    cin >> s;
+    cin >> t;
+    int t_size = t.size();
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != t[t_size - (i+1)])
+        {
+            count ++;
+        }
+    }
+    if(count > 0){
+        cout << "NO" << endl;
+    }
+    if(count == 0){
+        cout << "YES" << endl;
+    }
     return 0;
 }
