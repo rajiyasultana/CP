2017ca15716b5fccdcdd3a5e94234a638e1bb6ce
Rajiya Sultana <rssumu04@gmail.com>
2024-06-23 23:29:51 +0600

    Update Function.cpp

--- Day-22/Function.cpp
+++ Day-22/Function.cpp
@@ -1,6 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+int main()
+{
+    long long int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n / 2 << endl;
+    }
+    else
+    {
+        cout << (n / 2 - n) << endl;
+    }
+    /* Time limit exceed
+    for (int i = 1; i <= n; i++)
+    {
+        int function = pow(-1, i) * i;
+        sum += function;
+    }
+    cout << sum;*/
+
+    /*Time limit exceed
+    for (int i = 1; i <= n; i++)
+    {
+        if(i%2 != 0){
+            sum = sum - i;
+        }
+        else{
+            sum = sum + i;
+        }
+    }
+    cout << sum << endl;*/
+
     return 0;
 }
