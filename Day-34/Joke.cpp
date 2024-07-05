a90d79efdf4e7e9dcdc236a5c3971368d5305c18
Rajiya Sultana <rssumu04@gmail.com>
2024-07-05 23:13:53 +0600

    Update Joke.cpp

--- Day-34/Joke.cpp
+++ Day-34/Joke.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
-#include<string.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    string guest, host, letters, s1;
+    cin >> guest;
+    cin >> host;
+    cin >> letters;
+
+    s1 = guest + host;
+    sort(s1.begin(), s1.end());
+
+    sort(letters.begin(), letters.end());
+
+    if (s1 == letters)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
