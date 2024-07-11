d2283ee4ac0394a52501b3294fad26647e50444d
Rajiya Sultana <rssumu04@gmail.com>
2024-07-11 09:20:54 +0600

    Update New_year.cpp

--- Day-39/New_year.cpp
+++ Day-39/New_year.cpp
@@ -3,26 +3,28 @@ using namespace std;
 int main()
 {
     int n, k, count = 0;
-    
-    cin >> n >> k;
-    int arr[n+1];
-    arr[0] = 0;
-    
-    for(int i = 1; i<=n;i++){
-        arr[i] = i*5;
 
+    cin >> n >> k;
+    int arr[n + 1];
+    arr[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        arr[i] = i * 5;
     }
     int check = k;
 
-    for(int i = 1; i<=n; i++){
-        if(check<=240){
-            check+=arr[i];
-            if(check>240){
+    for (int i = 1; i <= n; i++)
+    {
+        if (check <= 240)
+        {
+            check += arr[i];
+            if (check > 240)
+            {
                 break;
             }
             count++;
         }
-        
     }
     cout << count << endl;
     return 0;
