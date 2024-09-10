9bafc335a29b73bf5b9713eca433022738d6e08b
Rajiya Sultana <rssumu04@gmail.com>
2024-09-10 23:35:20 +0600

    Create Sereja.cpp

---
+++ Day-46/Sereja.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+
+    sort(arr[n].begin, arr[n].end );
+
+    int S = 
+    return 0;
+}
