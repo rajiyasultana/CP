13c13f3e3b062c21098f08c2b96da81e07be6b7e
Rajiya Sultana <rssumu04@gmail.com>
2024-09-19 10:31:29 +0600

    Create Marathon.cpp

---
+++ Day-47/Marathon.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin >> t;
+
+    while (t--){
+        int arr[4], count = 0;
+    for(int i =0; i< 4; i++){
+        cin >> arr[i];
+    }
+    int a = arr[0];
+    for(int i = 1; i<4; i++){
+        if(arr[0] < arr[i]){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    }
+    
+    
+}
