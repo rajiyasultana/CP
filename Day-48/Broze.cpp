da66490e3098275ec9dffb0867c8b41389334892
Rajiya Sultana <rssumu04@gmail.com>
2024-09-20 21:07:22 +0600

    Create Broze.cpp

---
+++ Day-48/Broze.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string b, result = "";
+    cin >> b;
+
+    for (int i = 0; i<b.length(); i++){
+        if(b[i] == '.'){
+            result += "0";
+        }
+        else if(b[i] == '-'){
+            if(b[i+1]=='.'){
+                result += "1";
+                i++;
+            }
+            else if(b[i+1] == '-'){
+                result += "2";
+                i++;
+            }
+        }
+    }
+
+    cout << result << endl;
+    return 0;
+}
