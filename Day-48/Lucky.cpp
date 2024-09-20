153eaa21de18763913a6653f438981989b8b2910
Rajiya Sultana <rssumu04@gmail.com>
2024-09-20 21:41:53 +0600

    Create Lucky.cpp

---
+++ Day-48/Lucky.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+    cin >> s;
+
+    int sum1 = s[0] + s[1] + s[2];
+    int sum2 = s[3] + s[4] + s[5];
+
+    if (sum1 == sum2){
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    }
+    
+
+    return 0;
+}
